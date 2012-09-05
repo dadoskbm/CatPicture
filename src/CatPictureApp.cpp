fad31c92c4aebee1187d9a977e98dd8521ee52f4
Brandon Dadosky <dadoskbm@muohio.edu>
2012-09-05 00:12:05 -0400

    Added triangle method, get method

--- src/CatPictureApp.cpp
+++ src/CatPictureApp.cpp
@@ -7,6 +7,7 @@
 #include "cinder/gl/Texture.h"
 #include "cinder/ImageIo.h"
 
+
 #define WIDTH 800
 #define HEIGHT 600
 
@@ -20,17 +21,19 @@ class CatPictureApp : public AppBasic {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
-	bool modify(Color8u* color, int x, int y);
 	void drawLine(int xI, int yI, int xF, int yF, Color8u* color);
 	void drawRectangle(int xA, int yA, int xB, int yB, Color8u* line, Color8u* fill);
 	void drawCircle(int xC, int yC, int r, Color8u* line, Color8u* fill);
-	void copy(int x1, int x2, int y1, int y2, int xF, int yF);
+	//void copy(int x, int y, int l, int w, int xF, int yF);
+	void drawTriangle(int x1, int y1, int x2, int y2,int x3,int y3, Color8u* line);
 
   private:
+	bool modify(Color8u* color, int x, int y);
+	uint8_t* get(int x, int y);
+
 	Surface* surface;
 	uint8_t* dataArr;
 	math<int>* intMath;
-	
 };
 
 void CatPictureApp::setup()
@@ -41,6 +44,8 @@ void CatPictureApp::setup()
 
 	dataArr = surface->getData();
 
+
+	//Begin test cases
 	for(int x = 0; x < WIDTH; x++)
 	{
 		for(int y = 0; y < HEIGHT; y++)
@@ -51,9 +56,37 @@ void CatPictureApp::setup()
 	
 	drawLine(0,0,800,600,new Color8u(0,0,0));
 	drawRectangle(200, 210, 48, 92, new Color8u(0,0,0),new Color8u(255,0,0));
+	drawCircle(200, 300, 100, new Color8u(0,0,0), new Color8u(0, 0, 255));
+	drawTriangle(35, 135, 210, 411, 510, 500, new Color8u(0,0,0));
 }
 
-
+/**
+* Draws a circle.
+* Params:
+	xC = X-coordinate of the center of the circle
+	yC = Y-coordinate of the center of the circle
+	r = Radius of the circle
+	line = Color object containing color information for the outside line
+	fill = Color of the fill. If this value is 0, the circle won't be filled.
+*/
+void CatPictureApp::drawCircle(int xC, int yC, int r, Color8u* line, Color8u* fill)
+{
+	double dist;
+	for(int x = 1; x < WIDTH; x++)
+	{
+		for(int y = 1; y < HEIGHT; y++)
+		{
+			//Calculate the distance from the center, if it equals (or is close to)
+			// the radius, the pixel becomes part of the circle.
+			dist = math<double>().sqrt((xC - x) * (xC - x) + (yC - y) * (yC - y));
+			if(math<double>().abs(r - dist) < 0.5)
+				modify(line, x, y);
+			else if(dist < r && fill != 0) //TODO: Fill isn't exactly right, and stays toward the right of the circle. Might be a rounding issue.
+				modify(fill, x, y);
+		}
+	}
+			
+}
 
 void CatPictureApp::mouseDown( MouseEvent event )
 {
@@ -104,6 +137,77 @@ void CatPictureApp::drawLine(int xI, int yI, int xF, int yF, Color8u* color)
 			y += sy;
 		}
 	}
+}
+
+/**
+* Retrieves an array of color values at a given pixel
+* Params:
+	x = X-coordinate to get pixel data from
+	y = Y-coordinate to get pixel data from
+* Returns:
+	A pointer to a uint8_t array containing, in order, the red, green, and
+	blue values for the pixel.
+*/
+uint8_t* CatPictureApp::get(int x, int y)
+{
+	int start = 3 * (x + y * WIDTH);
+	uint8_t* arr = new uint8_t[3];
+	arr[0] = dataArr[start];
+	arr[1] = dataArr[start + 1];
+	arr[2] = dataArr[start + 2];
+	return arr;
+}
+
+//TODO: Doesn't work...
+///**
+//* copies a rectangular area from one area to another.
+//* params:
+//	xa= x-coordinate to start at
+//	ya = y-coordinate to start at
+//	l = length of the area to copy
+//	w = width of the area to copy
+//	xf = x-coordinate of the upper-left corner of where to paste to.
+//	yf = y-coordinate of above.
+//*/
+//void catpictureapp::copy(int x, int y, int l, int w, int xf, int yf)
+//{
+//	uint8_t* arr = new uint8_t[l * w * 3];
+//	int count = 0;
+//	for(int i = y; i < y + l; i++)
+//	{
+//		for(int j = x; j < x + w; j++, count += 3)
+//		{
+//			uint8_t* arrpixel = get(j, i);
+//			arr[count] = arrpixel[0];
+//			arr[count + 1] = arrpixel[1];
+//			arr[count + 2] = arrpixel[2];
+//			
+//		}
+//	}
+//	count = 0;
+//	for(int i = yf; i < y + l; i++)
+//	{
+//		for(int j = xf; y < x + w; j++, count += 3)
+//		{
+//			dataarr[j + i * 3] = arr[count];
+//			dataarr[j + i * 3 + 1] = arr[count + 1];
+//			dataarr[j + i * 3 + 2] = arr[count + 2];
+//		}
+//	}
+//}
+
+/**
+* Draws a triangle.
+* Params: 
+	x1, y1, x2, y2, x3, y3 = Points of the triangle
+	line = Color of the line
+*/
+void CatPictureApp::drawTriangle(int x1, int y1, int x2, int y2,int x3,int y3, Color8u* line)
+{
+	drawLine(x1, y1, x2, y2, line);
+	drawLine(x1, y1, x3, y3, line);
+	drawLine(x2, y2, x3, y3, line);
+
 }
 
 /**
